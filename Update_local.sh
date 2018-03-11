e42e25f8b5aab0dace80551b542c13e851b4c148
Nikolay <nicklukk98@gmail.com>
2018-03-11 19:57:58 +0200

    modify bash script to automaticly open venv

--- Update_local.sh
+++ Update_local.sh
@@ -1,4 +1,5 @@
+C:\\.virtualenv\\TwigYourHealth\\Scripts\\activate
 git pull origin master
 pip install -r requirements.txt
 python manage.py reset_staging
-python manage.py runserver
+python manage.py runserver 8000
