61de5c991b72f8cacbed8f1dfe12a2a4bce5b73e
marakaci <mykyta.chernenko@nure.ua>
2018-05-26 22:14:03 +0300

    redis url

--- TwigYourHealth/settings.py
+++ TwigYourHealth/settings.py
@@ -20,10 +20,12 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 63279)],
+            "hosts": [
+                "redis://h:p570b412a20b87e2a84e1f98031515320a74d257a3c291716aeea796185260e43@ec2-18-207-51-51.compute-1.amazonaws.com:11499"
+            ],
         },
     },
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
