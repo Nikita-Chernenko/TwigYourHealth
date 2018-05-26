3049b9fc43c4ffd3d2f73fd3c335e57278018855
marakaci <mykyta.chernenko@nure.ua>
2018-05-26 14:26:30 +0300

    settings

--- TwigYourHealth/asgi.py
+++ TwigYourHealth/asgi.py
@@ -2,6 +2,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TwigYourHealth.base_settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TwigYourHealth.settings")
 django.setup()
 application = get_default_application()
