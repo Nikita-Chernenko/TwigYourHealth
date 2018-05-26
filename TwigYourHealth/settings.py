22d1171f3c6bd08acb34fe617cdadc0107770818
marakaci <mykyta.chernenko@nure.ua>
2018-05-26 13:20:35 +0300

    redis url

--- TwigYourHealth/settings.py
+++ TwigYourHealth/settings.py
@@ -1,13 +1,13 @@
 from .base_settings import *
 import dj_database_url
 
-...
+
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
-...
+
 
 DATABASES = {
     'default': {
@@ -20,7 +20,14 @@ DATABASES = {
     }
 }
 
-...
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("h:p570b412a20b87e2a84e1f98031515320a74d257a3c291716aeea796185260e43@ec2-18-207-51-51.compute-1.amazonaws.com", 11499)],
+        },
+    },
+}
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
