814cbd0873ad1fe1297eeef9155be4319f8db14c
marakaci <mykyta.chernenko@nure.ua>
2018-05-28 00:32:21 +0300

    kets

--- TwigYourHealth/private_settings.py
+++ TwigYourHealth/private_settings.py
@@ -1,4 +1,4 @@
 PUSHBULLEY_KEY = 'o.9be9NZYtuyzAiGuk1h0VuhDvgSVN11ar'
 SECRET_KEY = '3xrsenq(r=3es9gajotczr0d2w(=d+qjj4o=2#@++*+r0(r8-4'
-AWS_S3_ACCESS_KEY_ID = "AKIAJES67JKWUXXM6F5Q"  # Your S3 Access Key
-AWS_S3_SECRET_ACCESS_KEY = "1daDfWxcMzxlcB+Dh/186x8sJkG2vqqU+BIrzlzc"  # Your S3 Secret
+AWS_S3_ACCESS_KEY_ID = "AKIAJTMQWGJULMRCDBUQ"  # Your S3 Access Key
+AWS_S3_SECRET_ACCESS_KEY = "46filocLYThfkLVEgq4tj/QoI8No5i6jgZb5EN5n"  # Your S3 Secret
