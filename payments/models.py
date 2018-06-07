b3a70d28426d0d34a4a4e24d2a4314f7981ae823
marakaci <mykyta.chernenko@nure.ua>
2018-06-07 23:14:18 +0300

    merged models

--- payments/models.py
+++ payments/models.py
@@ -18,6 +18,10 @@ class Order(models.Model):
     interaction = GenericForeignKey('content_type', 'object_id')
     sum = models.PositiveIntegerField('sum to pay')
     payed = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now=True)
+
+    # AWARE this is a db loop but it's impossible to filter by it. Don't set it manually
+    patient = models.ForeignKey(Patient, on_delete=models.PROTECT, blank=True, null=True)
 
     class Meta:
         unique_together = [['object_id', 'content_type']]
@@ -34,39 +38,45 @@ class Order(models.Model):
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
         # calculate sum of the interaction
-        content_type_changed = getattr(self, '__original_content_type', False) and \
+        content_type_changed = getattr(self, '__original_content_type', True) and \
                                self.__original_content_type != self.content_type
-        object_id_changed = getattr(self, '__original_object_id', False) and \
+        object_id_changed = getattr(self, '__original_object_id', True) and \
                             self.__original_object_id != self.object_id
         if content_type_changed or object_id_changed:
             model = self.content_type.model_class()
             model_id = self.object_id
             sum = None
+            patient = None
             if isinstance(model, Visit):
                 visit = Visit.objects.get(pk=model_id)
+                patient = visit.patient
                 doctor = visit.shift.timetable.doctor.privatedoctor
                 sum = doctor.visit_price
             elif isinstance(model, CallEntity):
                 call_entity = CallEntity.objects.get(pk=model_id)
+                patient = call_entity.patient
                 doctor = call_entity.doctor.privatedoctor
                 hours = (call_entity.end - call_entity.start).seconds // 3600
                 sum = doctor.hour_rate * hours
             elif isinstance(model, ChatEntity):
                 chat_entity = ChatEntity.objects.get(pk=model_id)
+                patient = chat_entity.patient
                 doctor = chat_entity.doctor.privatedoctor
                 hours = chat_entity.hours
                 sum = int(doctor.hour_rate * hours)
             self.sum = sum
+            self.patient = patient
         super(Order, self).save(force_insert, force_update, using, update_fields)
 
     def clean(self):
         # check that doctor is private
         if getattr(self, 'content_type', False) and self.object_id:
-            content_type_changed = getattr(self, '__original_content_type', False) and \
+            content_type_changed = getattr(self, '__original_content_type', True) or \
                                    self.__original_content_type != self.content_type
-            object_id_changed = getattr(self, '__original_object_id', False) and \
+            object_id_changed = getattr(self, '__original_object_id', True) or \
                                 self.__original_object_id != self.object_id
-            if content_type_changed or object_id_changed:
+            if (content_type_changed or object_id_changed) and \
+                    getattr(self, 'content_type', False) and self.object_id:
                 doctor = None
                 model = self.content_type.model_class()
                 model_id = self.object_id
@@ -83,34 +93,3 @@ class Order(models.Model):
                 if not doctor.is_private:
                     raise ValidationError('This doctor is public')
 
-
-class Payment(models.Model):
-    # AWARE this is a db loop but it's impossible to filter by it. Don't set it manually
-    patient = models.ForeignKey(Patient, on_delete=models.PROTECT, blank=True)
-
-    order = models.OneToOneField(Order, on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f'{self.patient} {self.order} {self.date}'
-
-    def save(self, force_insert=False, force_update=False, using=None,
-             update_fields=None):
-        order = getattr(self, 'order', False) and self.order
-        if not self.pk and order and getattr(order, 'content_type', False) and order.object_id:
-            model = order.content_type.model_class()
-            model_id = order.object_id
-            patient = None
-            if model is Visit:
-                visit = Visit.objects.get(pk=model_id)
-                patient = visit.patient
-            elif model is CallEntity:
-                call_entity = CallEntity.objects.get(pk=model_id)
-                patient = call_entity.patient
-            elif model is ChatEntity:
-                chat_entity = ChatEntity.objects.get(pk=model_id)
-                patient = chat_entity.patient
-            self.patient = patient
-            order.payed = True
-            order.save()
-        super(Payment, self).save(force_insert, force_update, using, update_fields)
