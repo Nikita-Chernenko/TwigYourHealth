903c783ccbcd074a870c2d46c2859b0f978b9ef1
marakaci <mykyta.chernenko@nure.ua>
2018-06-10 11:31:37 +0300

    about, contact

--- TwigYourHealth/urls.py
+++ TwigYourHealth/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     path('notifications/', include('notifications.urls')),
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
 ]
 
 if settings.DEBUG:
