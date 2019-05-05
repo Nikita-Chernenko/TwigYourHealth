bfb48bb5b680aae609bf106c6f8f1c4b4073f4f0
marakaci <mykyta.chernenko@nure.ua>
2019-05-05 11:29:27 +0300

    docker

---
+++ twig_your_health/static/js/call.js
@@ -0,0 +1,147 @@
+var CALL_REQUEST_ACTION = 'call_request';
+var CALL_ACCEPT_ACTION = 'call_accept';
+var CALL_DECLINE_ACTION = 'call_decline';
+var CALL_END_ACTION = 'call_end';
+var room;
+var webrtc;
+var $callRequestWrapper = $('#call-request-wrapper');
+var $callVideoWrapper = $('#call-window-wrapper');
+var withId;
+
+function call(CALL_REQUEST, CALL_ACCEPT, CALL_DECLINE, CALL_END) {
+    var ws_scheme = window.location.protocol === "https:" ? "wss" : "ws";
+    chatSocket = new WebSocket(
+        ws_scheme + '://' + window.location.host + '/call/');
+    chatSocket.onmessage = function (e) {
+        var message = JSON.parse(e.data);
+        console.log(message)
+        console.log(message['action']);
+        if (message['action'] === CALL_REQUEST_ACTION) {
+            withId = message['user_id'];
+            $callRequestWrapper.show();
+        }
+        else if (message['action'] === CALL_ACCEPT_ACTION) {
+            webrtc = new SimpleWebRTC({
+                // the id/element dom element that will hold "our" video
+                localVideoEl: 'video-self',
+                // the id/element dom element that will hold remote videos
+                remoteVideosEl: 'video-other',
+                // immediately ask for camera access
+                autoRequestMedia: true,
+                signalingOptions: {"force new connection": true},
+            });
+            room = message['room']
+            console.log(room);
+            $callRequestWrapper.hide();
+            $callVideoWrapper.show();
+            webrtc.on('readyToCall', function () {
+                webrtc.joinRoom(room);
+            });
+        }
+        else if (message['action'] === CALL_DECLINE_ACTION) {
+            $callVideoWrapper.hide();
+        }
+        else if (message['action'] === CALL_END_ACTION) {
+            if (webrtc) {
+                webrtc.stopLocalVideo();
+                webrtc.leaveRoom();
+                webrtc.disconnect();
+
+                $('#video-self').html('');
+                $('#video-other').html('');
+
+                $callVideoWrapper.hide();
+            }
+        }
+    }
+    ;
+
+    chatSocket.onclose = function () {
+        console.error('Chat socket closed unexpectedly');
+    };
+    chatSocket.onopen = function () {
+        console.log("Соединение установлено.");
+    };
+
+    chatSocket.onerror = function (error) {
+        console.log("Ошибка " + error.message);
+    };
+
+
+    $('.call-request').click(function () {
+        withId = parseInt($(this).parents('.chat-short').data('with-id'));
+        console.log(withId);
+        $.ajax({
+            url: CALL_REQUEST.replace('0', withId),
+            method: "POST",
+            success: function () {
+                $callRequestWrapper.hide();
+                $callVideoWrapper.show();
+            }
+        });
+        return false;
+    });
+    $('#accept-call-btn').click(function () {
+        console.log(withId);
+        $.ajax({
+            url: CALL_ACCEPT.replace('0', withId),
+            method: "POST",
+            success: function (message) {
+                webrtc = new SimpleWebRTC({
+                    // the id/element dom element that will hold "our" video
+                    localVideoEl: 'video-self',
+                    // the id/element dom element that will hold remote videos
+                    remoteVideosEl: 'video-other',
+                    // immediately ask for camera access
+                    autoRequestMedia: true,
+                    signalingOptions: {"force new connection": true}
+
+                });
+                room = message['room'];
+                console.log(room);
+                $callVideoWrapper.show();
+                webrtc.on('readyToCall', function () {
+                    // you can name it anything
+                    webrtc.createRoom(room, function (err, name) {
+                        console.log(err);
+                        console.log(name);
+                    });
+                });
+
+                $callRequestWrapper.hide();
+                $callVideoWrapper.show();
+            }
+        });
+
+
+    });
+    $('#decline-call-btn').click(function () {
+        console.log(room);
+        console.log('test');
+        $.ajax({
+            url: CALL_DECLINE.replace('0', withId),
+            method: "POST",
+            success: function () {
+                $callRequestWrapper.hide();
+            }
+        })
+    });
+    $('#end-call-btn').click(function () {
+        console.log(room);
+        console.log('test');
+        $.ajax({
+            url: CALL_END.replace('0', withId).replace('1', room),
+            method: "POST",
+            success: function () {
+                if (webrtc) {
+                    webrtc.stopLocalVideo();
+                    webrtc.leaveRoom();
+                    webrtc.disconnect();
+                }
+                $('#video-self').html('');
+                $('#video-other').html('');
+                $callVideoWrapper.hide();
+            }
+        })
+    })
+}
---
+++ twig_your_health/static/js/materialize_initialize.js
@@ -0,0 +1,17 @@
+function materialize_initiailize() {
+    // M.AutoInit();
+    $('.collapsible').collapsible();
+    $('select').formSelect();
+    $('.sidenav').sidenav();
+    $('.datepicker').datepicker({
+        selectMonths: true, // Creates a dropdown to control month
+        yearRange: [1900, 2030],// Creates a dropdown of 15 years to control year,
+        today: 'Today',
+        clear: 'Clear',
+        close: 'Ok',
+        closeOnSelect: false,
+        format: 'yyyy-mm-dd',
+        autoClose: true,
+    });
+    M.Tooltip.init($('.tooltipped'), {});
+}
